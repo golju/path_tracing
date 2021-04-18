4046b1825241fff286578b3261ed6c677dc8eece
Julia <golju@bk.ru>
2021-04-18 19:42:10 +0300

    Fix spaces

--- RenderTools.cpp
+++ RenderTools.cpp
@@ -8,12 +8,12 @@
 //================================ Free functions ==============================
 //==============================================================================
 double get_length(const cv::Vec3d &vec) {
-    return std::sqrt(
-            std::pow(vec[0], 2) + std::pow(vec[1], 2) + std::pow(vec[2], 2));
+  return std::sqrt(
+      std::pow(vec[0], 2) + std::pow(vec[1], 2) + std::pow(vec[2], 2));
 }
 
 cv::Vec3d get_normalized(const cv::Vec3d &vec) {
-    double length = get_length(vec);
+  double length = get_length(vec);
 
   if (length == 0) {
     return vec;
@@ -43,20 +43,20 @@ cv::Vec3d get_normalized(const cv::Vec3d &vec) {
 //}
 
 Ray fill_wavelength(Ray ray, std::vector<Light> lights) {
-    std::map<std::string, double> bright_coefs;
-    std::map<std::string, double> L;
+  std::map<std::string, double> bright_coefs;
+  std::map<std::string, double> L;
 
-    for (int i = 0; i < lights.size(); i++) {
-        for (auto &item : lights[i].spec_intensity) {
-            bright_coefs.insert(std::make_pair(item.first, 1));
-            L.insert(std::make_pair(item.first, 0));
-        }
+  for (int i = 0; i < lights.size(); i++) {
+    for (auto &item : lights[i].spec_intensity) {
+      bright_coefs.insert(std::make_pair(item.first, 1));
+      L.insert(std::make_pair(item.first, 0));
     }
+  }
 
-    ray.bright_coefficient = bright_coefs;
-    ray.L = L;
+  ray.bright_coefficient = bright_coefs;
+  ray.L = L;
 
-    return ray;
+  return ray;
 }
 
 //==============================================================================
@@ -68,16 +68,17 @@ Ray::Ray(const cv::Vec3d &origin, const cv::Vec3d &direction) : origin(origin),
 }
 
 Ray::Ray(const cv::Vec3d &new_origin, const cv::Vec3d &new_direction,
-         std::map<std::string, double> bright_coefs, std::map<std::string, double> L) :
-        origin(origin), direction(direction),
-        bright_coefficient(std::move(bright_coefs)), L(std::move(L)) {
+         std::map<std::string, double> bright_coefs,
+         std::map<std::string, double> L) :
+    origin(origin), direction(direction),
+    bright_coefficient(std::move(bright_coefs)), L(std::move(L)) {
 }
 
 //==============================================================================
 //================================ Material ====================================
 //==============================================================================
 Material::Material(std::map<std::string, double> rgb_Kd_color) : rgb_Kd_color(
-        std::move(rgb_Kd_color)) {
+    std::move(rgb_Kd_color)) {
 }
 
 //==============================================================================
@@ -97,7 +98,7 @@ Triangle::Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1,
                    const cv::Vec3d &v2,
                    const int &object_id,
                    const Material *material) :
-        v0(v0), v1(v1), v2(v2), object_id(object_id), material(material) {
+    v0(v0), v1(v1), v2(v2), object_id(object_id), material(material) {
 }
 
 cv::Vec3d Triangle::getNormal() const {
@@ -120,62 +121,62 @@ cv::Vec3d Triangle::getNormalByObserver(const cv::Vec3d &observer) const {
 }
 
 bool Triangle::intersect(const Ray &ray, double &t) const {
-    cv::Vec3d e1 = v1 - v0;
-    cv::Vec3d e2 = v2 - v0;
-    cv::Vec3d pvec = ray.direction.cross(e2);
-    double det = e1.dot(pvec);
+  cv::Vec3d e1 = v1 - v0;
+  cv::Vec3d e2 = v2 - v0;
+  cv::Vec3d pvec = ray.direction.cross(e2);
+  double det = e1.dot(pvec);
 
-    if (det < 1e-8 && det > -1e-8) {
-        return false;
-    }
+  if (det < 1e-8 && det > -1e-8) {
+    return false;
+  }
 
-    double inv_det = 1 / det;
-    cv::Vec3d tvec = ray.origin - v0;
-    double u = tvec.dot(pvec) * inv_det;
-    if (u < 0 || u > 1) {
-        return false;
-    }
+  double inv_det = 1 / det;
+  cv::Vec3d tvec = ray.origin - v0;
+  double u = tvec.dot(pvec) * inv_det;
+  if (u < 0 || u > 1) {
+    return false;
+  }
 
-    cv::Vec3d qvec = tvec.cross(e1);
-    double v = ray.direction.dot(qvec) * inv_det;
-    if (v < 0 || v + u > 1) {
-        return false;
-    }
+  cv::Vec3d qvec = tvec.cross(e1);
+  double v = ray.direction.dot(qvec) * inv_det;
+  if (v < 0 || v + u > 1) {
+    return false;
+  }
 
-    t = e2.dot(qvec) * inv_det;
-    return t > 1e-8;
+  t = e2.dot(qvec) * inv_det;
+  return t > 1e-8;
 }
 
 //==============================================================================
 //================================ Light =======================================
 //==============================================================================
 Light::Light(const cv::Vec3d &new_position, double new_total_flux) : position(
-        new_position), total_intensity(new_total_flux) {
+    new_position), total_intensity(new_total_flux) {
 }
 
 Light::Light(const cv::Vec3d &new_position, double new_total_flux,
              std::map<std::string, double> new_spec_intensity) : position(
-        new_position), total_intensity(new_total_flux), spec_intensity(std::move(
-        new_spec_intensity)) {
+    new_position), total_intensity(new_total_flux), spec_intensity(std::move(
+    new_spec_intensity)) {
 }
 
 //==============================================================================
 //================================ Camera ======================================
 //==============================================================================
 Camera::Camera(int width, int height, double fov, const cv::Vec3d &origin)
-        : width(width), height(height), fov(fov), origin(origin) {
+    : width(width), height(height), fov(fov), origin(origin) {
 }
 
 int Camera::getWidth() const {
-    return width;
+  return width;
 }
 
 int Camera::getHeight() const {
-    return height;
+  return height;
 }
 
 double Camera::getFov() const {
-    return fov;
+  return fov;
 }
 
 //FIXME: Rename this method
@@ -186,58 +187,59 @@ void Camera::getPicture() {
 //================================ Scene =======================================
 //==============================================================================
 Scene::Scene() {
-    // Materials
-    //Хардкод, так как в файле нет спектральных свойств поверхностей
-    std::map<std::string, double> rgb_Kd_color_brs_0;
-    rgb_Kd_color_brs_0.insert(std::make_pair("r", 0.321));
-    rgb_Kd_color_brs_0.insert(std::make_pair("g", 0.174));
-    rgb_Kd_color_brs_0.insert(std::make_pair("b", 0.045));
-    materials.emplace_back(Material(rgb_Kd_color_brs_0));
+  // Materials
+  //Хардкод, так как в файле нет спектральных свойств поверхностей
+  std::map<std::string, double> rgb_Kd_color_brs_0;
+  rgb_Kd_color_brs_0.insert(std::make_pair("r", 0.321));
+  rgb_Kd_color_brs_0.insert(std::make_pair("g", 0.174));
+  rgb_Kd_color_brs_0.insert(std::make_pair("b", 0.045));
+  materials.emplace_back(Material(rgb_Kd_color_brs_0));
 
-    std::map<std::string, double> rgb_Kd_color_brs_1;
-    rgb_Kd_color_brs_1.insert(std::make_pair("r", 0.03));
-    rgb_Kd_color_brs_1.insert(std::make_pair("g", 0.06));
-    rgb_Kd_color_brs_1.insert(std::make_pair("b", 0.018));
-    materials.emplace_back(Material(rgb_Kd_color_brs_1));
+  std::map<std::string, double> rgb_Kd_color_brs_1;
+  rgb_Kd_color_brs_1.insert(std::make_pair("r", 0.03));
+  rgb_Kd_color_brs_1.insert(std::make_pair("g", 0.06));
+  rgb_Kd_color_brs_1.insert(std::make_pair("b", 0.018));
+  materials.emplace_back(Material(rgb_Kd_color_brs_1));
 
-    std::map<std::string, double> rgb_Kd_color_brs_2;
-    rgb_Kd_color_brs_2.insert(std::make_pair("r", 0.057));
-    rgb_Kd_color_brs_2.insert(std::make_pair("g", 0.008));
-    rgb_Kd_color_brs_2.insert(std::make_pair("b", 0.000));
-    materials.emplace_back(Material(rgb_Kd_color_brs_2));
+  std::map<std::string, double> rgb_Kd_color_brs_2;
+  rgb_Kd_color_brs_2.insert(std::make_pair("r", 0.057));
+  rgb_Kd_color_brs_2.insert(std::make_pair("g", 0.008));
+  rgb_Kd_color_brs_2.insert(std::make_pair("b", 0.000));
+  materials.emplace_back(Material(rgb_Kd_color_brs_2));
 
-    std::map<std::string, double> rgb_Kd_color_brs_3;
-    rgb_Kd_color_brs_3.insert(std::make_pair("r", 0.107));
-    rgb_Kd_color_brs_3.insert(std::make_pair("g", 0.058));
-    rgb_Kd_color_brs_3.insert(std::make_pair("b", 0.015));
-    materials.emplace_back(Material(rgb_Kd_color_brs_3));
+  std::map<std::string, double> rgb_Kd_color_brs_3;
+  rgb_Kd_color_brs_3.insert(std::make_pair("r", 0.107));
+  rgb_Kd_color_brs_3.insert(std::make_pair("g", 0.058));
+  rgb_Kd_color_brs_3.insert(std::make_pair("b", 0.015));
+  materials.emplace_back(Material(rgb_Kd_color_brs_3));
 
-    std::map<std::string, double> rgb_Kd_color_brs_4;
-    rgb_Kd_color_brs_4.insert(std::make_pair("r", 0.107));
-    rgb_Kd_color_brs_4.insert(std::make_pair("g", 0.058));
-    rgb_Kd_color_brs_4.insert(std::make_pair("b", 0.015));
-    materials.emplace_back(Material(rgb_Kd_color_brs_4));
+  std::map<std::string, double> rgb_Kd_color_brs_4;
+  rgb_Kd_color_brs_4.insert(std::make_pair("r", 0.107));
+  rgb_Kd_color_brs_4.insert(std::make_pair("g", 0.058));
+  rgb_Kd_color_brs_4.insert(std::make_pair("b", 0.015));
+  materials.emplace_back(Material(rgb_Kd_color_brs_4));
 
-    // Lights
-    //Хардкод, так как в файле нет свойств и положения источника света
-    int total_intensity = 1445872; // W/sr
-    double Itotal = 2100;
-    double Isim_r = 1624.35;
-    double Isim_g = 425.67;
-    double Isim_b = 49.98;
+  // Lights
+  //Хардкод, так как в файле нет свойств и положения источника света
+  int total_intensity = 1445872; // W/sr
+  double Itotal = 2100;
+  double Isim_r = 1624.35;
+  double Isim_g = 425.67;
+  double Isim_b = 49.98;
 
-    std::map<std::string, double> spec_intensity;
-    spec_intensity.insert(
-            std::make_pair("r", total_intensity * (Isim_r / Itotal)));
-    spec_intensity.insert(
-            std::make_pair("g", total_intensity * (Isim_g / Itotal)));
-    spec_intensity.insert(
-            std::make_pair("b", total_intensity * (Isim_b / Itotal)));
+  std::map<std::string, double> spec_intensity;
+  spec_intensity.insert(
+      std::make_pair("r", total_intensity * (Isim_r / Itotal)));
+  spec_intensity.insert(
+      std::make_pair("g", total_intensity * (Isim_g / Itotal)));
+  spec_intensity.insert(
+      std::make_pair("b", total_intensity * (Isim_b / Itotal)));
 
-    Light a = Light(cv::Vec3d(278, 545, -279.5), 1445872,
-                    spec_intensity);
-    lights.push_back(a); //Было Light(cv::Vec3d(278, -279.5, 548.7), 1445872, spec_intensity)
-    //278, 548.7, -279.5
+  Light a = Light(cv::Vec3d(278, 545, -279.5), 1445872,
+                  spec_intensity);
+  lights.push_back(
+      a); //Было Light(cv::Vec3d(278, -279.5, 548.7), 1445872, spec_intensity)
+  //278, 548.7, -279.5
 }
 
 void Scene::setNewCamera(const Camera &camera) {
@@ -281,20 +283,20 @@ Ray Scene::fireRay(Ray &ray) {
 bool Scene::intersect(const Ray &ray, cv::Vec3d &hit, cv::Vec3d &N,
                       Material &material) {
 
-    double min_distance_to_triangle = std::numeric_limits<double>::max();
+  double min_distance_to_triangle = std::numeric_limits<double>::max();
 
-    for (auto &triangle : triangles) {
-        double distance_to_triangle = -1.0;
-        if (triangle.intersect(ray, distance_to_triangle) &&
-            distance_to_triangle < min_distance_to_triangle) {
-            min_distance_to_triangle = distance_to_triangle;
-            hit = ray.origin + ray.direction * distance_to_triangle;
-            N = triangle.getNormalByObserver(ray.origin - hit);
-            material = *triangle.material;
-        }
+  for (auto &triangle : triangles) {
+    double distance_to_triangle = -1.0;
+    if (triangle.intersect(ray, distance_to_triangle) &&
+        distance_to_triangle < min_distance_to_triangle) {
+      min_distance_to_triangle = distance_to_triangle;
+      hit = ray.origin + ray.direction * distance_to_triangle;
+      N = triangle.getNormalByObserver(ray.origin - hit);
+      material = *triangle.material;
     }
+  }
 
-    return min_distance_to_triangle < std::numeric_limits<double>::max();
+  return min_distance_to_triangle < std::numeric_limits<double>::max();
 }
 
 int Scene::load(const std::string &path_to_file) {
@@ -368,54 +370,54 @@ int Scene::load(const std::string &path_to_file) {
 
 void Scene::render() {
 
-    for (auto &camera : cameras) {
-        int width = camera.getWidth();
-        int height = camera.getHeight();
-        double fov = camera.getFov();
+  for (auto &camera : cameras) {
+    int width = camera.getWidth();
+    int height = camera.getHeight();
+    double fov = camera.getFov();
 
-        std::vector<Ray> framebuffer(width * height);
+    std::vector<Ray> framebuffer(width * height);
 
 #pragma omp parallel for
-        for (long long i = 0; i < height; i++) {
-            for (long long j = 0; j < width; j++) {
-                double x =
-                        -(2 * (j + 0.5) / (double) width - 1) * tan(fov / 2.) * width /
-                        (double) height;
-                double y = -(2 * (i + 0.5) / (double) height - 1) * tan(fov / 2.);
-                cv::Vec3d direction = get_normalized(cv::Vec3d(x, y, -1)); //Было -1
+    for (long long i = 0; i < height; i++) {
+      for (long long j = 0; j < width; j++) {
+        double x =
+            -(2 * (j + 0.5) / (double) width - 1) * tan(fov / 2.) * width /
+            (double) height;
+        double y = -(2 * (i + 0.5) / (double) height - 1) * tan(fov / 2.);
+        cv::Vec3d direction = get_normalized(cv::Vec3d(x, y, -1)); //Было -1
 
-                Ray ray(cv::Vec3d(250, 300, 500),
-                        direction); //100, 200, 100 //250, 300, 500
-                ray = fill_wavelength(ray, lights);
+        Ray ray(cv::Vec3d(250, 300, 500),
+                direction); //100, 200, 100 //250, 300, 500
+        ray = fill_wavelength(ray, lights);
 
-                framebuffer[j + i * width] = fireRay(ray);
-            }
-        }
-
-        // FIXME: Change save path
-        std::ofstream fout("../data/results.txt");
-
-        std::vector<std::string> wavelengths;
-        wavelengths.emplace_back("r");
-        wavelengths.emplace_back("g");
-        wavelengths.emplace_back("b");
-
-        for (int k = 0; k < wavelengths.size(); k++) {
-            fout << "wavelength" << " " << wavelengths[k] << std::endl;
-            for (long long i = 0; i < height; i++) {
-                for (long long j = 0; j < width; j++) {
-                    if (j == 0) {
-                        fout << framebuffer[j + i * width].L.find(wavelengths[k])->second;
-                    } else {
-                        fout << " "
-                             << framebuffer[j + i * width].L.find(wavelengths[k])->second;
-                    }
-                }
-                fout << std::endl;
-            }
-            fout << "\n";
-        }
-
-        fout.close();
+        framebuffer[j + i * width] = fireRay(ray);
+      }
     }
+
+    // FIXME: Change save path
+    std::ofstream fout("../data/results.txt");
+
+    std::vector<std::string> wavelengths;
+    wavelengths.emplace_back("r");
+    wavelengths.emplace_back("g");
+    wavelengths.emplace_back("b");
+
+    for (int k = 0; k < wavelengths.size(); k++) {
+      fout << "wavelength" << " " << wavelengths[k] << std::endl;
+      for (long long i = 0; i < height; i++) {
+        for (long long j = 0; j < width; j++) {
+          if (j == 0) {
+            fout << framebuffer[j + i * width].L.find(wavelengths[k])->second;
+          } else {
+            fout << " "
+                 << framebuffer[j + i * width].L.find(wavelengths[k])->second;
+          }
+        }
+        fout << std::endl;
+      }
+      fout << "\n";
+    }
+
+    fout.close();
+  }
 }
--- RenderTools.h
+++ RenderTools.h
@@ -18,9 +18,9 @@ double get_length(const cv::Vec3d &vec);
 //================================ Point =======================================
 //==============================================================================
 struct Point {
-    double x = 0;
-    double y = 0;
-    double z = 0;
+  double x = 0;
+  double y = 0;
+  double z = 0;
 
   // FIXME: На будущее
 //  double n_x = 0;
@@ -33,16 +33,17 @@ struct Point {
 //==============================================================================
 class Ray {
 public:
-    Ray() = default;
-    Ray(const cv::Vec3d &origin, const cv::Vec3d &direction);
-    Ray(const cv::Vec3d &new_origin, const cv::Vec3d &new_direction,
-        std::map<std::string, double> bright_coefs, std::map<std::string, double> L);
+  Ray() = default;
+  Ray(const cv::Vec3d &origin, const cv::Vec3d &direction);
+  Ray(const cv::Vec3d &new_origin, const cv::Vec3d &new_direction,
+      std::map<std::string, double> bright_coefs,
+      std::map<std::string, double> L);
 
 public:
-    cv::Vec3d origin;
-    cv::Vec3d direction;
-    std::map<std::string, double> bright_coefficient; //spec
-    std::map<std::string, double> L;
+  cv::Vec3d origin;
+  cv::Vec3d direction;
+  std::map<std::string, double> bright_coefficient; //spec
+  std::map<std::string, double> L;
 };
 
 //==============================================================================
@@ -51,12 +52,12 @@ public:
 // FIXME: Maybe smth is private
 class Material {
 public:
-    Material() = default;
-    Material(std::map<std::string, double> rgb_Kd_color);
+  Material() = default;
+  Material(std::map<std::string, double> rgb_Kd_color);
 
 public:
-    //double kd = -1; // Diffuse reflection coef 1 = 100%
-    std::map<std::string, double> rgb_Kd_color; // int wavelength, Kd color
+  //double kd = -1; // Diffuse reflection coef 1 = 100%
+  std::map<std::string, double> rgb_Kd_color; // int wavelength, Kd color
 };
 
 //==============================================================================
@@ -65,22 +66,22 @@ public:
 // FIXME: Maybe smth is private
 class Triangle {
 public:
-    Triangle() = default;
+  Triangle() = default;
 
-    Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2);
-    Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2,
-             const int &object_id);
-    Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2,
-             const int &object_id,
-             const Material *material);
-    cv::Vec3d getNormal() const;
-    cv::Vec3d getNormalByObserver(const cv::Vec3d &observer) const;
-    bool intersect(const Ray &ray, double &t) const;
+  Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2);
+  Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2,
+           const int &object_id);
+  Triangle(const cv::Vec3d &v0, const cv::Vec3d &v1, const cv::Vec3d &v2,
+           const int &object_id,
+           const Material *material);
+  cv::Vec3d getNormal() const;
+  cv::Vec3d getNormalByObserver(const cv::Vec3d &observer) const;
+  bool intersect(const Ray &ray, double &t) const;
 
 public:
-    cv::Vec3d v0, v1, v2;
-    int object_id = -1;
-    const Material *material;
+  cv::Vec3d v0, v1, v2;
+  int object_id = -1;
+  const Material *material;
 };
 
 //==============================================================================
@@ -88,15 +89,15 @@ public:
 //==============================================================================
 struct Light {
 public:
-    Light() = default;
-    Light(const cv::Vec3d &new_position, double new_total_flux);
-    Light(const cv::Vec3d &new_position, double new_total_flux,
-          std::map<std::string, double> new_spec_intensity);
+  Light() = default;
+  Light(const cv::Vec3d &new_position, double new_total_flux);
+  Light(const cv::Vec3d &new_position, double new_total_flux,
+        std::map<std::string, double> new_spec_intensity);
 
 public:
-    cv::Vec3d position;
-    double total_intensity = 0; // W/sr
-    std::map<std::string, double> spec_intensity; // int wavelength, double intensity
+  cv::Vec3d position;
+  double total_intensity = 0; // W/sr
+  std::map<std::string, double> spec_intensity; // int wavelength, double intensity
 };
 
 //==============================================================================
@@ -104,20 +105,20 @@ public:
 //==============================================================================
 class Camera {
 public:
-    Camera(int width, int height, double fov, const cv::Vec3d &origin);
+  Camera(int width, int height, double fov, const cv::Vec3d &origin);
 
-    void getPicture();
+  void getPicture();
 
-    int getWidth() const;
-    int getHeight() const;
-    double getFov() const;
+  int getWidth() const;
+  int getHeight() const;
+  double getFov() const;
 
 private:
-    const int width = -1;
-    const int height = -1;
-    const double fov = -1.0;
+  const int width = -1;
+  const int height = -1;
+  const double fov = -1.0;
 
-    const cv::Vec3d origin = {-1.0, -1.0, -1.0};
+  const cv::Vec3d origin = {-1.0, -1.0, -1.0};
 };
 
 //==============================================================================
@@ -125,23 +126,23 @@ private:
 //==============================================================================
 class Scene {
 public:
-    Scene();
-    void render();
-    Ray fireRay(Ray &ray);
-    int load(const std::string &path_to_file);
-    void setNewCamera(const Camera &camera);
+  Scene();
+  void render();
+  Ray fireRay(Ray &ray);
+  int load(const std::string &path_to_file);
+  void setNewCamera(const Camera &camera);
 
 private:
-    bool
-    intersect(const Ray &ray, cv::Vec3d &hit, cv::Vec3d &N, Material &material);
+  bool
+  intersect(const Ray &ray, cv::Vec3d &hit, cv::Vec3d &N, Material &material);
 
 private:
-    std::vector<cv::Vec3d> points;
-    std::vector<Triangle> triangles;
-    std::vector<int> object_id;
-    std::vector<Light> lights;
-    std::vector<Material> materials;
-    std::vector<Camera> cameras;
+  std::vector<cv::Vec3d> points;
+  std::vector<Triangle> triangles;
+  std::vector<int> object_id;
+  std::vector<Light> lights;
+  std::vector<Material> materials;
+  std::vector<Camera> cameras;
 };
 
 #endif //RENDER_TOOLS_H
